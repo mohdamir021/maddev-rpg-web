96d998bac9d0f639fd053ca682716898c91d2582
mohdamir021 <amir.ibrahim201200@gmail.com>
2024-03-31 20:31:42 +0800

    feat: pattern v2 code block

--- src/components/actions/PlayerControlHook.tsx
+++ src/components/actions/PlayerControlHook.tsx
@@ -15,19 +15,6 @@ export default function PlayerControlHook() {
 
   return (
     <Flex flexDir={"row"} gap={"12px"}>
-      {/* <Box p={"30px"} bgColor={"lightcyan"} maxW={"300px"}>
-        <Text>Player Control Effects</Text>
-        <Box my={"3px"} p={"2px"} bgColor={"whitesmoke"}>
-          <Text fontWeight={700}>Health : {health}</Text>
-          <Text fontWeight={700}>Mana : {mana}</Text>
-        </Box>
-        <Stack spacing={"2px"} my={"3px"} p={"2px"}>
-          <Button onClick={() => heal(20)}>Heal</Button>
-          <Button onClick={() => attack(100)}>Attack</Button>
-          <Button onClick={() => useMagic(10)}>Use Magic</Button>
-          <Button onClick={() => usePotion(50)}>Use Potion</Button>
-        </Stack>
-      </Box> */}
       <Box p={"30px"} bgColor={"yellowgreen"} maxW={"300px"}>
         <Text>Player Control Effects V2</Text>
         <Box my={"3px"} p={"2px"} bgColor={"whitesmoke"}>
--- src/components/models/PlayerCodeBlocks.tsx
+++ src/components/models/PlayerCodeBlocks.tsx
@@ -119,6 +119,121 @@ export default function PlayerControlBoard() {
           />
         </Box>
       </Box>
+      <Box>
+        <Text fontSize={"20px"} fontWeight={800}>
+          Hook V1
+        </Text>
+        <Box my={2}>
+          <CodeBlock
+            theme={a11yDark}
+            language="typescript"
+            text={`// src:/hooks/usePlayerState.tsx
+/* eslint-disable @typescript-eslint/no-confusing-void-expression */
+import { useAppToast } from "@/utils/useAppToast";
+import { useState, useEffect } from "react";
+
+interface StatePatternV1<T> {
+\t  get: () => T;
+\t  set: React.Dispatch<React.SetStateAction<T>>;
+\t  effect: (callback?: (value: T) => void, dependencies?: unknown[]) => void;
+}
+
+const generateStatePattern = ([value, setValue]: [
+\t  any,
+\t  React.Dispatch<React.SetStateAction<any>>,
+]): StatePatternV1<any> => {
+\t  return {
+\t\t    get: () => value,
+\t\t    set: setValue?.bind(null),
+\t\t    effect: (callback?: (currValue?: typeof value) => void, deps?: unknown[]) => {
+\t\t      useEffect(() => {
+\t\t       callback?.(value);
+\t\t      }, [...(deps ?? []), value]);
+\t\t    },
+\t  };
+};
+
+interface PlayerHookReturn {
+\t  health: StatePatternV1<number>;
+\t  mana: StatePatternV1<number>;
+\t  // heal: (value: number) => void;
+\t  attack: (damage: number) => void;
+\t  useMagic: (cost: number) => void;
+\t  // usePotion: (value: number) => void;
+}
+
+export default function usePlayerState(): PlayerHookReturn {
+\t  // Initialize state
+\t  const { showToast } = useAppToast();
+\t  const health = generateStatePattern(useState(300));
+\t  const mana = generateStatePattern(useState(50));
+\t
+\t  // Return Value
+\t  return {
+\t\t    health,
+\t\t    mana,
+\t\t    attack: (damage: number) => {
+\t\t\t      health.get() < 51
+\t\t\t\t        ? showToast({ title: "Low health, go heal", status: "error" })
+\t\t\t\t        : showToast({ title: \`Damage dealt: \${damage} pts\`, status: "warning" });
+\t\t\t      health.get() > 50 && health.set(health.get() - 5);
+\t\t    },
+\t\t    useMagic: (cost: number) =>
+\t\t\t      cost > mana.get()
+\t\t\t\t        ? showToast({ title: "Not enough mana", status: "error" })
+\t\t\t\t        : mana.set(mana.get() - cost),
+\t  };
+}
+`}
+          />
+        </Box>
+      </Box>
+      <Box>
+        <Text fontSize={"20px"} fontWeight={800}>
+          Component V3
+        </Text>
+        <Box my={2}>
+          <CodeBlock
+            theme={a11yDark}
+            language="typescript"
+            text={`// src:/components/PlayerControlBoard.tsx
+/* eslint-disable @typescript-eslint/no-confusing-void-expression */
+import usePlayerState from "@/hooks/usePlayerState";
+import { Box, Button, Flex, Stack, Text } from "@chakra-ui/react";
+import React from "react";
+
+export default function PlayerControlHook() {
+\t  const { health, mana, attack, useMagic } = usePlayerState();
+\t
+\t  const heal = (value: number) => {
+\t\t    health.set(health.get() + value);
+\t  };
+\t  const usePotion = (value: number) => {
+\t\t    mana.set(mana.get() + value);
+\t  };
+\t
+\t  return (
+\t\t    <Flex flexDir={"row"} gap={"12px"}>
+\t\t\t      <Box p={"30px"} bgColor={"yellowgreen"} maxW={"300px"}>
+\t\t\t\t        <Text>Player Control Effects V2</Text>
+\t\t\t\t        <Box my={"3px"} p={"2px"} bgColor={"whitesmoke"}>
+\t\t\t\t\t          <Text fontWeight={700}>Health : {health.get()}</Text>
+\t\t\t\t\t          <Text fontWeight={700}>Mana : {mana.get()}</Text>
+\t\t\t\t        </Box>
+\t\t\t\t        <Stack spacing={"2px"} my={"3px"} p={"2px"}>
+\t\t\t\t\t          <Button onClick={() => heal(20)}>Heal</Button>
+\t\t\t\t\t          <Button onClick={() => attack(100)}>Attack</Button>
+\t\t\t\t\t          <Button onClick={() => useMagic(10)}>Use Magic</Button>
+\t\t\t\t\t          <Button onClick={() => usePotion(50)}>Use Potion</Button>
+\t\t\t\t        </Stack>
+\t\t\t      </Box>
+\t\t    </Flex>
+\t  );
+}
+`}
+          />
+        </Box>
+      </Box>
     </SimpleGrid>
   );
 }
